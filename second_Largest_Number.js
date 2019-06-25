609c5e7d4d3ca531944f6a899d0a3072efebcf2d
sreenidhithallam <sreenidhithallam@gmail.com>
2019-06-25 23:31:05 +0530

    secondLargestNumber

---
+++ second_Largest_Number.js
@@ -0,0 +1,22 @@
+// Problem Statement:: To find the second largest number
+
+function secondLarge(arr) {
+  let i = 0;
+let largest = -1;
+let secondLargest = -1;
+for(i; i <= arr.length; i++) {
+  if(arr[i] > largest) {
+    secondLargest = largest;
+    largest = arr[i];
+  } else if(arr[i] < largest && arr[i] > secondLargest) {
+    secondLargest = arr[i];
+  }
+}
+  return secondLargest;
+}
+
+// Sample input :: [8,5,4,19,20,21,7,6,3]
+var arr = [8,5,4,19,20,21,7,6,3];
+// Sample output:: 20
+
+secondLarge(arr);
